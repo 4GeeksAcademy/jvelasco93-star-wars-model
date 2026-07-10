fea9f26ddd13906980c7a1cd6fa53e2e25f21079
Julio Cesar Velasco <116952866+jvelasco93@users.noreply.github.com>
2026-07-10 07:44:41 +0000

    Implementados endpoints delete

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,12 +1,9 @@
 from flask import Blueprint, jsonify
 from http import HTTPStatus
-from models import db, User
+from models import Character, Planet, Vehicle, db, User
 from utils import APIException
 
 users_bp = Blueprint("users", __name__)
-
-# SELECT user
-# FROM users;
 
 
 @users_bp.route("/users", methods=["GET"])
@@ -57,6 +54,87 @@ def get_user_favorites(user_id: int):
             "characters": [planet.serialize() for planet in user.favorite_characters],
             "vehicles": [vehicle.serialize() for vehicle in user.favorite_vehicles],
         }), HTTPStatus.OK
+    except APIException:
+        raise
+    except Exception as e:
+        raise APIException(
+            str(e), status_code=HTTPStatus.INTERNAL_SERVER_ERROR)
+
+
+@users_bp.route('/users/<int:user_id>/favorites/planets/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet(user_id: int, planet_id: int):
+    try:
+        user = db.session.get(User, user_id)
+        if not user:
+            raise APIException(
+                "User not found", status_code=HTTPStatus.NOT_FOUND)
+
+        planet = db.session.get(Planet, planet_id)
+        if not planet:
+            raise APIException("Planet not found",
+                               status_code=HTTPStatus.NOT_FOUND)
+
+        if planet not in user.favorite_planets:
+            raise APIException("Planet is not in favorites",
+                               status_code=HTTPStatus.NOT_FOUND)
+
+        user.favorite_planets.remove(planet)
+        db.session.commit()
+        return jsonify({"msg": "Planet removed from favorites"}), HTTPStatus.OK
+    except APIException:
+        raise
+    except Exception as e:
+        raise APIException(
+            str(e), status_code=HTTPStatus.INTERNAL_SERVER_ERROR)
+
+
+@users_bp.route("/users/<int:user_id>/favorites/characters/<int:character_id>", methods=["DELETE"])
+def delete_favorite_character(user_id, character_id):
+    try:
+        user = db.session.get(User, user_id)
+        if not user:
+            raise APIException(
+                "User not found", status_code=HTTPStatus.NOT_FOUND)
+
+        character = db.session.get(Character, character_id)
+        if not character:
+            raise APIException("Character not found",
+                               status_code=HTTPStatus.NOT_FOUND)
+
+        if character not in user.favorite_characters:
+            raise APIException("Character is not in favorites",
+                               status_code=HTTPStatus.NOT_FOUND)
+
+        user.favorite_characters.remove(character)
+        db.session.commit()
+        return jsonify({"msg": "Character removed from favorites"}), HTTPStatus.OK
+    except APIException:
+        raise
+    except Exception as e:
+        raise APIException(
+            str(e), status_code=HTTPStatus.INTERNAL_SERVER_ERROR)
+
+
+@users_bp.route("/users/<int:user_id>/favorites/vehicles/<int:vehicle_id>", methods=["DELETE"])
+def delete_favorite_vehicle(user_id, vehicle_id):
+    try:
+        user = db.session.get(User, user_id)
+        if not user:
+            raise APIException(
+                "User not found", status_code=HTTPStatus.NOT_FOUND)
+
+        vehicle = db.session.get(Vehicle, vehicle_id)
+        if not vehicle:
+            raise APIException("Vehicle not found",
+                               status_code=HTTPStatus.NOT_FOUND)
+
+        if vehicle not in user.favorite_vehicles:
+            raise APIException("Vehicle is not in favorites",
+                               status_code=HTTPStatus.NOT_FOUND)
+
+        user.favorite_vehicles.remove(vehicle)
+        db.session.commit()
+        return jsonify({"msg": "Vehicle removed from favorites"}), HTTPStatus.OK
     except APIException:
         raise
     except Exception as e:
